19b104b454e9806313894461d4b52d2ec1c98b25
GurveerSingh1 <81967740+GurveerSingh1@users.noreply.github.com>
2021-08-02 13:34:33 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCqoYPBS3xBY-Wzth7bUTkdqdisdUcbWHM",
+    authDomain: "class-test-cae88.firebaseapp.com",
+    projectId: "class-test-cae88",
+    storageBucket: "class-test-cae88.appspot.com",
+    messagingSenderId: "91711557936",
+    appId: "1:91711557936:web:516faea1392b44c29738f5",
+    measurementId: "G-J82QBHP43E"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
